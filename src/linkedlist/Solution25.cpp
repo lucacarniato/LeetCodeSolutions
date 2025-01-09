6b8e90ce6154e2ec707ef561caf71952869536b5
Luca Carniato <luca.carniato@deltares.nl>
2025-01-09 22:28:27 +0100

    Improve solution 25

--- src/linkedlist/Solution25.cpp
+++ src/linkedlist/Solution25.cpp
@@ -7,6 +7,49 @@ class Solution25
 public:
     ListNode* reverseKGroup(ListNode* head, int k)
     {
+        if (!head || k <= 1)
+            return head;
+
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* prevGroupEnd = &dummy;
+
+        while (true)
+        {
+            ListNode* groupStart = prevGroupEnd->next;
+            ListNode* groupEnd = prevGroupEnd;
+            for (int i = 0; i < k && groupEnd; ++i)
+            {
+                groupEnd = groupEnd->next;
+            }
+
+            if (!groupEnd)
+                break;
+
+            ListNode* nextGroupStart = groupEnd->next;
+            ListNode* prev = nextGroupStart;
+            ListNode* curr = groupStart;
+            while (curr != nextGroupStart)
+            {
+                ListNode* next = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = next;
+            }
+
+            prevGroupEnd->next = groupEnd;
+            prevGroupEnd = groupStart;
+        }
+
+        return dummy.next;
+    }
+};
+
+class Solution25Second
+{
+public:
+    ListNode* reverseKGroup(ListNode* head, int k)
+    {
         ListNode* cn = head;
         ListNode* chunkHead = head;
         std::vector<ListNode*> revertedListChunks;
