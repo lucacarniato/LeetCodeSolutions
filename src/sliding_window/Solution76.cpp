98511a54de7eb1d4b4ee3b0b969cc0b5ff40f64b
Luca Carniato <luca.carniato@deltares.nl>
2025-01-12 12:39:41 +0100

    improve solution 76

--- src/sliding_window/Solution76.cpp
+++ src/sliding_window/Solution76.cpp
@@ -15,48 +15,47 @@ public:
         }
 
         constexpr int vocabulary_size = 256;
-        std::array<int, vocabulary_size> freqMap{0};
+        std::array<int, vocabulary_size> tFreq{0};
 
-        int charToMatch = 0;
+        int numberOfMatchesToAchive = 0;
         for (int i = 0; i < t.size(); ++i)
         {
             unsigned char pos = t[i];
-            freqMap[pos]++;
+            tFreq[pos]++;
         }
         for (int i = 0; i < vocabulary_size; ++i)
         {
-            if (freqMap[i] > 0)
+            if (tFreq[i] > 0)
             {
-                charToMatch++;
+                numberOfMatchesToAchive++;
             }
         }
 
         std::queue<size_t> q;
         size_t minSizeOfSubstring = s.size();
         string result = "";
-        std::array<int, vocabulary_size> freqSubstr{0};
-        int matched = 0;
+        std::array<int, vocabulary_size> sFreq{0};
+        int matchesSoFar = 0;
         for (int i = 0; i < s.size(); ++i)
         {
             unsigned char pos = s[i];
-            if (freqMap[pos] == 0)
+            if (tFreq[pos] == 0)
             {
                 continue;
             }
 
-            freqSubstr[pos]++;
-            if (freqSubstr[pos] == freqMap[pos])
-            {
-                matched++;
-            }
             q.push(i);
-
-            // matching found, evaluate min string
-            if (matched == charToMatch)
+            sFreq[pos]++;
+            if (sFreq[pos] == tFreq[pos])
             {
-                while (freqSubstr[s[q.front()]] > freqMap[s[q.front()]])
+                matchesSoFar++;
+            }
+
+            if (matchesSoFar == numberOfMatchesToAchive)
+            {
+                while (sFreq[s[q.front()]] > tFreq[s[q.front()]])
                 {
-                    freqSubstr[s[q.front()]]--;
+                    sFreq[s[q.front()]]--;
                     q.pop();
                 }
 
