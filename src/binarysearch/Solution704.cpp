bd4b148851d541504a08678619f8c13e521910dd
Luca Carniato <luca.carniato@deltares.nl>
2025-01-19 21:10:45 +0100

    Add solution 704

---
+++ src/binarysearch/Solution704.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+
+using namespace std;
+
+class Solution704
+{
+public:
+    int search(vector<int>& nums, int target)
+    {
+
+        int l = 0;
+        int r = nums.size() - 1;
+        while (l <= r)
+        {
+            int mid = (r + l) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            if (nums[mid] > target)
+            {
+                r = mid - 1;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
