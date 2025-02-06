d531cde0cefa4578b824fb38d96046e4b36932fa
Luca Carniato <luca.carniato@deltares.nl>
2025-02-06 19:41:07 +0100

    Improve solution 853

--- src/stack/Solution853.cpp
+++ src/stack/Solution853.cpp
@@ -5,64 +5,37 @@ using namespace std;
 
 class Solution853
 {
-    std::tuple<vector<int>, vector<int>> sortDescenting(vector<int>& position, vector<int>& speed)
-    {
-        // Create a vector of pairs (element, index)
-        std::vector<std::pair<int, size_t>> elementIndexPairs(position.size());
-        for (size_t i = 0; i < position.size(); ++i)
-        {
-            elementIndexPairs[i] = std::make_pair(position[i], i);
-        }
-
-        // Sort the vector of pairs based on the elements
-        std::sort(elementIndexPairs.begin(), elementIndexPairs.end(),
-                  [](const std::pair<int, size_t>& a, const std::pair<int, size_t>& b)
-                  {
-                      return a.first < b.first;
-                  });
-
-        // Extract the indices from the sorted pairs
-        std::vector<size_t> sortedIndices(position.size());
-        std::vector<int> newPosition(position.size());
-        std::vector<int> newSpeed(position.size());
-        for (size_t i = 0; i < elementIndexPairs.size(); ++i)
-        {
-            const auto index = elementIndexPairs[i].second;
-            newPosition[i] = position[index];
-            newSpeed[i] = speed[index];
-        }
-
-        return {newPosition, newSpeed};
-    }
-
 public:
-    int carFleet(int target, vector<int>& p, vector<int>& s)
+    int carFleet(int target, vector<int>& position, vector<int>& speed)
     {
-
-        if (p.size() == 1)
+        if (position.empty())
         {
-            return 1;
+            return 0;
         }
 
-        auto [position, speed] = sortDescenting(p, s);
-
-        vector<double> timeToTarget(position.size(), 0.0);
+        // sort in descending order
+        std::vector<std::pair<int, int>> positionsSpeed;
         for (size_t i = 0; i < position.size(); ++i)
         {
-            timeToTarget[i] = (target - position[i]) / static_cast<double>(speed[i]);
+            positionsSpeed.emplace_back(position[i], speed[i]);
+        }
+        std::sort(positionsSpeed.begin(), positionsSpeed.end(), [](const auto& a, const auto& b)
+                  { return a.first > b.first; });
+
+        std::vector<double> timeToTravel;
+        for (const auto& [pos, spd] : positionsSpeed)
+        {
+            timeToTravel.push_back((double)(target - pos) / spd);
         }
 
-        std::stack<double> st;
-        st.push(timeToTarget[timeToTarget.size() - 1]);
-        for (int i = timeToTarget.size() - 2; i >= 0; --i)
+        std::stack<double> s;
+        for (double tt : timeToTravel)
         {
-            const auto& tt = st.top();
-
-            if (timeToTarget[i] > tt)
+            if (s.empty() || tt > s.top())
             {
-                st.push(timeToTarget[i]);
+                s.push(tt);
             }
         }
-        return st.size();
+        return s.size();
     }
 };
