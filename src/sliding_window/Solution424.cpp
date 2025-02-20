1e0ea9aae454c88ae84316b5499c131a8462dff9
Luca Carniato <luca.carniato@deltares.nl>
2025-02-20 22:06:29 +0100

    Add Solution 424

---
+++ src/sliding_window/Solution424.cpp
@@ -0,0 +1,25 @@
+class Solution424
+{
+public:
+    int characterReplacement(string s, int k) 
+    {
+        int maxLength = 0;
+        int maxCount = 0;
+        int start = 0;
+        vector<int> count(26, 0); 
+
+        for (int end = 0; end < s.length(); ++end) {
+            count[s[end] - 'A']++;
+            maxCount = max(maxCount, count[s[end] - 'A']);
+
+            while ((end - start + 1) - maxCount > k) {
+                count[s[start] - 'A']--;
+                start++;
+            }
+
+            maxLength = max(maxLength, end - start + 1);
+        }
+        
+        return maxLength;
+    }
+};
