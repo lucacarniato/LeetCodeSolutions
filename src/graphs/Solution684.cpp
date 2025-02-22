df5e3009d4407b6d93473e98bf4b0859f42a1cb6
Luca Carniato <luca.carniato@deltares.nl>
2025-02-22 16:19:10 +0100

    Solve problem 684 using union find

--- src/graphs/Solution684.cpp
+++ src/graphs/Solution684.cpp
@@ -1,3 +1,68 @@
+class SolutionUnionFind {
+
+    std::vector<int> parents_;
+    std::vector<int> ranks_;
+
+    int find(int n)
+    {
+        if(n == parents_[n])
+        {
+            return n;
+        }
+        parents_[n] = find(parents_[n]); // path compression
+        return parents_[n];
+    }
+
+    bool Union(int u, int v)
+    {
+        int root_u =  find(u);
+        int root_p =  find(v);
+
+        if(root_p==root_u)
+        {
+            return false;
+        }
+
+        if (ranks_[root_p] > ranks_[root_u])
+        {
+            parents_[root_u] = root_p;
+            ranks_[root_p] += ranks_[root_u];
+        }
+        else
+        {
+            parents_[root_p] = root_u;
+            ranks_[root_u] += ranks_[root_p];
+        }
+
+        return true;
+    }
+
+
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) 
+    {        
+        // Construct the graph
+        int n = edges.size();
+        parents_.resize(n + 1);
+        for(int i = 0; i < n;++i)
+        {
+            parents_[i] = i;
+        }
+        ranks_.resize(n + 1 ,0);
+        
+        for(const auto& e: edges)
+        {
+            const auto u = e[0];
+            const auto v = e[1];
+            if(!Union(u,v))
+            {
+                return e;
+            }
+        }
+        return {};        
+    }
+};
+
 class Solution684 {
 
     bool HasCycle(std::vector<vector<int>>& adj,
