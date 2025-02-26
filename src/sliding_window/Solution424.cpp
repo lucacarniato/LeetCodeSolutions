077d40e25e2c28ac50620dc1adf9fa7118a7a8ff
Luca Carniato <luca.carniato@deltares.nl>
2025-02-26 21:48:50 +0100

    Improve solutions

--- src/sliding_window/Solution424.cpp
+++ src/sliding_window/Solution424.cpp
@@ -1,25 +1,30 @@
+#include <vector>
+using namespace std;
+
 class Solution424
 {
 public:
-    int characterReplacement(string s, int k) 
+    int characterReplacement(std::string s, int k)
     {
         int maxLength = 0;
         int maxCount = 0;
         int start = 0;
-        vector<int> count(26, 0); 
+        vector<int> count(26, 0);
 
-        for (int end = 0; end < s.length(); ++end) {
+        for (int end = 0; end < s.length(); ++end)
+        {
             count[s[end] - 'A']++;
             maxCount = max(maxCount, count[s[end] - 'A']);
 
-            while ((end - start + 1) - maxCount > k) {
+            while ((end - start + 1) - maxCount > k)
+            {
                 count[s[start] - 'A']--;
                 start++;
             }
 
             maxLength = max(maxLength, end - start + 1);
         }
-        
+
         return maxLength;
     }
 };
--- src/stack/Solution732.cpp
+++ src/stack/Solution732.cpp
@@ -1,22 +1,26 @@
-class Solution {
+#include <vector>
+using namespace std;
+class Solution
+{
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) 
+    vector<int> dailyTemperatures(vector<int>& temperatures)
     {
         std::stack<std::tuple<int, int>> s;
         vector<int> results(temperatures.size(), 0);
-        for(int i=0; i < temperatures.size() ;++i)
+        for (int i = 0; i < temperatures.size(); ++i)
         {
             int new_temp = temperatures[i];
-            if(s.empty())
+            if (s.empty())
             {
                 s.push({new_temp, i});
                 continue;
             }
 
-            while(!s.empty())
+            while (!s.empty())
             {
                 const auto [old_temp, old_i] = s.top();
-                if (new_temp <= old_temp) break; 
+                if (new_temp <= old_temp)
+                    break;
                 results[old_i] = i - old_i;
                 s.pop();
             }
@@ -24,7 +28,5 @@ public:
         }
 
         return results;
-
-        
     }
 };
