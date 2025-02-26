9d07e48f2cff772e7513ccec785eeaa273b1be7c
Luca Carniato <luca.carniato@deltares.nl>
2025-02-26 22:01:01 +0100

    Solution 3232

---
+++ src/graphs/Solution323.cpp
@@ -0,0 +1,42 @@
+class Solution323 {
+public:
+    int countComponents(int n, vector<vector<int>>& edges) 
+    {
+        std::vector<std::vector<int>> adj(n);
+        for(int i=0; i < edges.size();++i)
+        {
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+        }
+		
+		std::vector<bool> visited(n, false);
+		int result = 0;
+		for(int i=0; i < n;++i)
+		{
+			if(visited[i]==true)
+			{
+				continue;
+			}
+			result++;
+			std::queue<int> q;
+			q.push(i);
+			
+			while(!q.empty())
+			{
+				const auto cn = q.front();
+				q.pop();
+				visited[cn] = true;
+				
+				for(const auto& nei: adj[cn])
+				{
+					if(visited[nei])
+					{
+						continue;
+					}
+					q.push(nei);
+				}
+			}	
+		}
+		return result;
+    }
+};
