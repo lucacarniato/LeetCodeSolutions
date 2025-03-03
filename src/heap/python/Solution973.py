3921685e689ce8f4d154d1c8410f585a918152f4
Luca Carniato <luca.carniato@deltares.nl>
2025-03-03 23:31:33 +0100

    Add solution 973

---
+++ src/heap/python/Solution973.py
@@ -0,0 +1,17 @@
+import heapq
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+  
+        minheap = []
+
+        for p in points:
+            distance =p[0] * p[0] + p[1] * p[1]
+            if len(minheap) < k:
+                heapq.heappush(minheap, (-distance, p))
+            elif -minheap[0][0] > distance:
+                heapq.heappop(minheap)
+                heapq.heappush(minheap, (-distance, p))
+
+        result = [p[1] for p in minheap]
+        return result 
